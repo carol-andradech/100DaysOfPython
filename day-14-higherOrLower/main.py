c84bda6c05f209c3359f175da17475581b5681a9
Carol Andrade <carol.andradechagas@gmail.com>
2023-09-28 10:59:58 -0300

    day-14-equalAccount

--- day-14-higherOrLower/main.py
+++ day-14-higherOrLower/main.py
@@ -11,26 +11,30 @@ def randomAccount():
 
 
 def compareFollowers(optionA, optionB, answer):
-    if answer == "A" and optionA['follower_count'] > optionB['follower_count']:
-        return optionA
-    elif answer == "B" and optionB['follower_count'] > optionA['follower_count']:
-        return optionB
+    if answer == "a" and optionA['follower_count'] > optionB['follower_count']:
+        return True
+    elif answer == "b" and optionB['follower_count'] > optionA['follower_count']:
+        return True
     else:
         return False
 
 
 print(logo)
 optionA = randomAccount()
+optionB = optionA
 while not end:
-
-    optionB = randomAccount()
+    while optionB == optionA:
+        optionB = randomAccount()
+        print(optionB)
 
     print(
         f"Compare A: {optionA['name']}, a {optionA['description']}, from {optionA['country']}")
     print(vs)
     print(
         f"Agaisnt B: {optionB['name']}, a {optionB['description']}, from {optionB['country']}")
-    answer = input("Who has more followers? Type 'A' or 'B': ")
+    print(optionA['follower_count'])
+    print(optionB['follower_count'])
+    answer = input("Who has more followers? Type 'A' or 'B': ").lower()
 
     compare = compareFollowers(optionA, optionB, answer)
 
@@ -39,6 +43,6 @@ while not end:
         end = True
     else:
         print("\x1b[2J\x1b[H")
-        optionA = compare
+        optionA = optionB
         score += 1
         print(f"You're right! Current score: {score}")
