5f31afa1ce9dfee0e2082d4c9ba1b71709a03bbc
Carol Andrade <carol.andradechagas@gmail.com>
2023-10-06 17:04:27 -0300

    day-21

--- day-20-SnakeGame/main.py
+++ day-20-SnakeGame/main.py
@@ -9,9 +9,10 @@
 # Detect collision with tail
 
 from turtle import Turtle, Screen
-from snake import Snake
 import time
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -20,6 +21,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food= Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 # UP:90ª, LEFT:180ª, DOWN:270ª, RIGHT:0ª
@@ -33,13 +36,27 @@ while game_is_on:
     # Delay for 0.1s and then refresh the screen
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
 
-
-
+    #Detect colision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 
--- day-20-SnakeGame/snake.py
+++ day-20-SnakeGame/snake.py
@@ -15,11 +15,18 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle(shape="square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
 
     def move(self):
         # start , stop , step
